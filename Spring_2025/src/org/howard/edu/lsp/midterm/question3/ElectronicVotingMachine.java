5a64418b8ebb276a18be405f654d2e2f63624e2a
Avery Austin <avery.austin@bison.howard.edu>
2025-03-20 19:56:08 -0400

    EVM class

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question3/ElectronicVotingMachine.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question3;
+
+public class ElectronicVotingMachine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
