538fb66b0d516c0c15a3dde03f6830a28fcee151
Avery Austin <avery.austin@bison.howard.edu>
2025-03-20 19:27:45 -0400

    getters and setters, to string, bookdriver

--- Spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -16,6 +16,48 @@ public class Book {
 		this.ISBN = ISBN;
 		this.yearPublished = yearPublished;
 	}
-	
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public int getYearPublished() {
+		return yearPublished;
+	}
+
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Book book = (Book) obj;
+		return ISBN.equals(book.ISBN) && author.equals(book.author);
+	}
+	@Override 
+	
+	public String toString() {
+		return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+	}
 }
