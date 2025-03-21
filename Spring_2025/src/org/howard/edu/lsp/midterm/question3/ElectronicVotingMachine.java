5438067b06c45fb1d3bf1356ecef55691b89caf5
Avery Austin <avery.austin@bison.howard.edu>
2025-03-20 20:03:19 -0400

    tally q4

--- Spring_2025/src/org/howard/edu/lsp/midterm/question3/ElectronicVotingMachine.java
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question3/ElectronicVotingMachine.java
@@ -1,10 +1,46 @@
 package org.howard.edu.lsp.midterm.question3;
 
-public class ElectronicVotingMachine {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ElectronicVotingMachine {
+	private Map<String, Integer> votes;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}		
+		public ElectronicVotingMachine(){
+			votes = new HashMap<>();
+		}
+		
+		public void addCandidate(String name) {
+			if (!votes.containsKey(name)) {
+				votes.put(name, 0);
+			}
+		}
+		
+		public boolean castVote(String name) {
+			if (votes.containsKey(name)) {
+				votes.put(name, votes.get(name) + 1);
+				return true;
+			}
+			return false;
+		}
+		
+		public String getWinner() {
+			String winner = null;
+			int maxVotes = 0;
+			
+			for (Map.Entry<String, Integer> entry: votes.entrySet()) {
+				if (entry.getValue() > maxVotes) {
+					maxVotes = entry.getValue();
+					winner = entry.getKey();
+				}
+			}
+			return (winner != null) ? winner + " WINS with " + maxVotes + " votes!!" : "No winner.";
 	}
-
+	
 }
+/* Code from https://www.geeksforgeeks.org/java-program-to-count-the-occurrence-of-each-character-in-a-string-using-hashmap/
+ and chat gpt*/
---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question3/Tally.java
@@ -0,0 +1,29 @@
+package org.howard.edu.lsp.midterm.question3;
+
+public class Tally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ElectronicVotingMachine vm = new ElectronicVotingMachine();
+		
+		vm.addCandidate("Alic");
+		vm.addCandidate("Bob");
+		vm.addCandidate("Charlie");
+		
+		vm.castVote("Alice");
+		vm.castVote("Alice");
+		vm.castVote("Bob");
+		vm.castVote("Charlie");
+		vm.castVote("Charlie");
+		vm.castVote("Charlie");
+		
+		boolean success = vm.castVote("Eve");
+		System.out.println("Vote for Eve successful? " + success);
+		
+		System.out.println("Winner: " + vm.getWinner());
+			
+		
+		
+	}
+
+}
