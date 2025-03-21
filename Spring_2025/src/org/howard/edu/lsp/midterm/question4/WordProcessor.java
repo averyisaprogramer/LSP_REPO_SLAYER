d12587718dc2847cbe93b897ac44c93ba53e7bd7
Avery Austin <avery.austin@bison.howard.edu>
2025-03-20 20:11:14 -0400

    q4 (acutal) part 1

--- Spring_2025/src/org/howard/edu/lsp/midterm/question4/WordProcessor.java
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question4/WordProcessor.java
@@ -1,10 +1,46 @@
 package org.howard.edu.lsp.midterm.question4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordProcessor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+    private String sentence; 
 
+ 
+    public WordProcessor(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public List<String> findLongestWords() {
+        List<String> longestWords = new ArrayList<>();
+        if (sentence == null || sentence.trim().isEmpty()) {
+            return longestWords; 
+        }
+
+        String[] words = sentence.split("\\s+"); 
+        int maxLength = 0;
+
+        for (String word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+            }
+        }
+
+        for (String word : words) {
+            if (word.length() == maxLength) {
+                longestWords.add(word);
+            }
+        }
+
+        return longestWords;
+    }
 }
+
+
+/* references https://stackoverflow.com/questions/70550937/finding-the-longest-word-arraylist-java
+and chat gpt */
