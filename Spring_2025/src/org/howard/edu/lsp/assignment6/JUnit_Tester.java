678ebf80653988e01994f3068494589a14e301c5
Avery Austin <avery.austin@bison.howard.edu>
2025-04-05 18:25:30 -0400

    renaming for the assignment.

---
+++ Spring_2025/src/org/howard/edu/lsp/assignment6/JUnit_Tester.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment6;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntegerSetTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
