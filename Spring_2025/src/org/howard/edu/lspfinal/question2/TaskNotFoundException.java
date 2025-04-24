ca8224148e79a2360aba2d3f7797f990439a740b
Avery Austin <avery.austin@bison.howard.edu>
2025-04-24 19:41:01 -0400

    third commit?

--- Spring_2025/src/org/howard/edu/lspfinal/question2/TaskNotFoundException.java
+++ Spring_2025/src/org/howard/edu/lspfinal/question2/TaskNotFoundException.java
@@ -1,10 +1,12 @@
 package org.howard.edu.lspfinal.question2;
 
-public class TaskNotFoundException {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+/**
+ * Exception thrown when a task is not found in the TaskManager.
+ */
+public class TaskNotFoundException extends Exception {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
 }
+/* code sourced from chat gpt
+ */
