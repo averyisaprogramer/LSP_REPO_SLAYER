f829985a57385e94ad0df3ef68664938e5fe8027
Avery Austin <avery.austin@bison.howard.edu>
2025-04-24 19:47:22 -0400

    first commit

---
+++ Spring_2025/src/org/howard/edu/lspfinal/question3/InventoryReport.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lspfinal.question3;
+
+/**
+ * Concrete class for generating Inventory Reports.
+ */
+public class InventoryReport extends Report {
+
+    @Override
+    public void formatData() {
+        System.out.println("Formatting inventory data...");
+    }
+}
+
+/* code sourced from chat gpt
+ */ 
