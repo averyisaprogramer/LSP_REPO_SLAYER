d117db97362d2dfa985f27f35f1715e8c79b9fc8
Avery Austin <avery.austin@bison.howard.edu>
2025-04-24 19:40:28 -0400

    second commit

--- Spring_2025/src/org/howard/edu/lspfinal/question2/DuplicateTaskException.java
+++ Spring_2025/src/org/howard/edu/lspfinal/question2/DuplicateTaskException.java
@@ -1,10 +1,13 @@
 package org.howard.edu.lspfinal.question2;
 
-public class DuplicateTaskException {
+public class DuplicateTaskException extends Exception {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
+	public DuplicateTaskException(String message) {
+		super(message);
+	}
 }
+/* code sourced from chat gpt
+ */
