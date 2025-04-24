54ccbf991ec9d17043226b4e39b608390f2a3807
Avery Austin <avery.austin@bison.howard.edu>
2025-04-24 19:46:40 -0400

    first commit

---
+++ Spring_2025/src/org/howard/edu/lspfinal/question3/SalesReport.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lspfinal.question3;
+
+/**
+ * Concrete class for generating Sales Reports.
+ */
+public class SalesReport extends Report {
+
+    @Override
+    public void formatData() {
+        System.out.println("Formatting sales data...");
+    }
+}
+/* code sourced from chat gpt
+ */
