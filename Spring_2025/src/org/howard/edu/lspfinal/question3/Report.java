f4d2ef742e71419181882b17a8ce33f95d3c9ed9
Avery Austin <avery.austin@bison.howard.edu>
2025-04-24 20:21:26 -0400

    making sure

--- Spring_2025/src/org/howard/edu/lspfinal/question3/Report.java
+++ Spring_2025/src/org/howard/edu/lspfinal/question3/Report.java
@@ -1,10 +1,29 @@
 package org.howard.edu.lspfinal.question3;
 
-public class Report {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+/**
+ * Abstract class defining the template for report generation.
+ */
+public abstract class Report {
+    
+    // Template method defining the general algorithm for generating a report
+    public final void generateReport() {
+        loadData();         // Common step
+        formatData();       // This step is overridden by subclasses
+        printReport();      // Common step
+    }
+    
+    // Concrete method shared by all reports (no need to override)
+    public void loadData() {
+        System.out.println("Loading data...");
+    }
+    
+    // Abstract method that must be implemented by subclasses for formatting data
+    public abstract void formatData();
+    
+    // Concrete method shared by all reports (no need to override)
+    public void printReport() {
+        System.out.println("Printing report.");
+    }
 }
+/* code sourced from chat gpt
+ */
